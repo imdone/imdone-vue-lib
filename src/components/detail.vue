f2eefb0e2fb97a6861b36bf9e05e832758525edf
Jesse <jesse@piascik.net>
2018-07-18 09:39:24 -0600

    Better fix for description

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -85,7 +85,7 @@ export default {
       const text = this.task.getText({stripMeta: true, sanitize: true, stripTags: true, stripContext: true})
       const regex = /((http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?)/
       const descriptionMD = description.join('\n').replace(regex, '<a href="$1">$1</a>')
-      const sep = (description.length > 0 && (!description[0].trim() || description[0] === '----')) ? `\n  \n` : ''
+      const sep = (description.length > 0 && (!description[0].trim() || description[0].trim().startsWith('-'))) ? `\n  \n` : ''
       const html = md.render(`${text}${sep}${descriptionMD}`)
       const $ = cheerio.load(html)
       $('a').each(function () {
