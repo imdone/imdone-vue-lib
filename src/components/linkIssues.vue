16fdf0a57aae1bda6246d28625471a54cd2e1337
Jesse <jesse@piascik.net>
2018-11-15 00:03:11 -0700

    Add newIssue component

--- src/components/linkIssues.vue
+++ src/components/linkIssues.vue
@@ -15,51 +15,45 @@
           b-table-column.is-narrow.has-text-centered(field="actions")
             a.is-small(v-on:click="unlinkIssue(props.row)" :title="`unlink ${ props.row.number }`")
               b-icon(pack="fa" icon="unlink" size="is-small")
-
-  .columns
-    .column.is-12.has-text-right(v-if="linkIssuesActive")
-      a.button.is-small(v-on:click="toggleLinkIssues" title="hide")
-        b-icon(pack="fa" icon="share-alt" size="is-small")
-        span Cancel
-    .column.is-12.has-text-right
-      a.button.is-small(v-if="showLinkIssues" v-on:click="toggleLinkIssues" title="link to issue")
-        b-icon(pack="fa" icon="share-alt" size="is-small")
-        span Link Issues
-  .columns(v-if="linkIssuesActive")
-    .column.is-9
-      b-field(custom-class="is-small")
-        b-select(placeholder="Filters" size="is-small")
-          option Open issues and PRs
-          option Your Issues
-          option Your PRs
-          option Assigned to you
-          option Mentioning you
-        b-input(type="search" :placeholder="defaultSearch" size="is-small" v-model="userSearch" @keyup.native.enter="searchIssues")
-    .column.is-3.has-text-right
-      button.button.is-success.is-small New Issue
-  .columns(v-if="linkIssuesActive && searchResults")
-    .column.is-12
-      b-table.is-size-7(:data="searchResults.items")
-        template(slot-scope="props")
-          b-table-column(field="number" label="Number")
-            a(:href="props.row.html_url" target="_blank") {{ props.row.number }}
-          b-table-column(field="title" label="Title")
-            a(:href="props.row.html_url" target="_blank") {{ props.row.title }}
-          b-table-column.is-narrow.has-text-centered(field="state" label="State")
-            b-tag.has-text-weight-bold(:class="stateText(props.row.state)")
-              octicon(:icon="stateIcon(props.row.state)")
-              span.is-capitalized &nbsp;{{ props.row.state }}
-          b-table-column.is-narrow.has-text-centered(field="actions")
-            a.is-small(v-if="!hasIssue(props.row)" v-on:click="linkIssue(props.row)" :title="`link ${ props.row.number }`")
-              b-icon(pack="fa" icon="link" size="is-small")
-            a.is-small(v-if="hasIssue(props.row)" v-on:click="unlinkIssue(props.row)" :title="`unlink ${ props.row.number }`")
-              b-icon(pack="fa" icon="unlink" size="is-small")
+  h3.subtitle.is-4.has-text-left Link Issues
+  newIssue(v-if="newIssueShown" :task="task" :repoURL="repoURL" :baseURL="baseURL" :allowUpdates="allowUpdates" v-on:cancel="toggleNewIssue")
+  .issue-search(v-else)
+    .columns
+      .column.is-9
+        b-field(custom-class="is-small")
+          b-select(placeholder="Filters" size="is-small")
+            option Open issues and PRs
+            option Your Issues
+            option Your PRs
+            option Assigned to you
+            option Mentioning you
+          b-input(type="search" :placeholder="defaultSearch" size="is-small" v-model="userSearch" @keyup.native.enter="searchIssues")
+      .column.is-3.has-text-right
+        button.button.is-success.is-small(v-on:click="toggleNewIssue") New Issue
+    .columns(v-if="searchResults")
+      .column.is-12
+        b-table.is-size-7(:data="searchResults.items")
+          template(slot-scope="props")
+            b-table-column(field="number" label="Number")
+              a(:href="props.row.html_url" target="_blank") {{ props.row.number }}
+            b-table-column(field="title" label="Title")
+              a(:href="props.row.html_url" target="_blank") {{ props.row.title }}
+            b-table-column.is-narrow.has-text-centered(field="state" label="State")
+              b-tag.has-text-weight-bold(:class="stateText(props.row.state)")
+                octicon(:icon="stateIcon(props.row.state)")
+                span.is-capitalized &nbsp;{{ props.row.state }}
+            b-table-column.is-narrow.has-text-centered(field="actions")
+              a.is-small(v-if="!hasIssue(props.row)" v-on:click="linkIssue(props.row)" :title="`link ${ props.row.number }`")
+                b-icon(pack="fa" icon="link" size="is-small")
+              a.is-small(v-if="hasIssue(props.row)" v-on:click="unlinkIssue(props.row)" :title="`unlink ${ props.row.number }`")
+                b-icon(pack="fa" icon="unlink" size="is-small")
 
 </template>
 <script>
 import Buefy from 'buefy'
 import axios from 'axios'
 import Octicon, { Octicons } from 'octicons-vue'
+import newIssue from '@/components/newIssue'
 import * as _ from 'lodash'
 
 export default {
@@ -72,19 +66,20 @@ export default {
     'b-field': Buefy.Field,
     'b-select': Buefy.Select,
     'b-input': Buefy.Input,
-    Octicon
+    Octicon,
+    newIssue
   },
   props: ['task', 'repoURL', 'baseURL', 'allowUpdates', 'searchIssuesURL'],
   data () {
     return {
       activeTab: 0,
       Octicons,
-      linkIssuesActive: false,
       defaultSearch: 'is:issue is:open',
       userSearch: null,
       searchResults: null,
       taskIssues: [],
-      repoFullName: null
+      repoFullName: null,
+      newIssueShown: false
     }
   },
   created () {
@@ -101,6 +96,9 @@ export default {
     }
   },
   methods: {
+    toggleNewIssue () {
+      this.newIssueShown = !this.newIssueShown
+    },
     searchIssues () {
       axios.get(`${this.searchIssuesURL}?per_page=8&q=${this.searchString}`)
       .then(response => {
@@ -112,9 +110,6 @@ export default {
     },
     stateText (state) {
       return state === 'open' ? 'is-success' : 'is-danger'
-    },
-    toggleLinkIssues () {
-      this.linkIssuesActive = !this.linkIssuesActive
     },
     linkIssue (issue) {
       this.task.addIssue(issue)
---
+++ src/components/newIssue.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+.new-issue
+  .columns
+    .column.is-12
+      b-field
+        b-input(placeholder="Title")
+      b-field
+        b-input(placeholder="Leave a comment" maxlength="200" type="textarea")
+  .columns
+    .column.is-6
+    .column.has-text-right
+      button.button.is-small(v-on:click="$emit('cancel')") Cancel
+    .column.has-text-right
+      button.button.is-success.is-small Submit new issue
+</template>
+<script>
+import Buefy from 'buefy'
+
+export default {
+  name: 'imdone-new-issue',
+  components: {
+    'b-field': Buefy.Field,
+    'b-input': Buefy.Input
+  },
+  props: ['task', 'repoURL', 'baseURL', 'allowUpdates'],
+  data () {
+    return {
+    }
+  },
+  created () {
+  }
+}
+</script>
