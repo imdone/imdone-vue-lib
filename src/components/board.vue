35c92a0f31dcc32acea942c4ec99e75392ccc6cb
imdone.io app user <jesse@piascik.net>
2018-12-14 16:48:06 -0500

    TODO comments updated by @imdoneio [ci skip] [imdone skip]

--- src/components/board.vue
+++ src/components/board.vue
@@ -48,7 +48,7 @@ import _ from 'lodash'
 export default {
   name: 'imdone-board',
   components: {List, Detail, Draggable, 'b-icon': Icon},
-  // DOING: Should accept a v-model **board** in the format `{config, lists}` where lists is a list of tasks in the format `{name, hidden, tasks}`
+  // DOING: Should accept a v-model **board** in the format `{config, lists}` where lists is a list of tasks in the format `{name, hidden, tasks}` id:40
   props: ['value', 'tasks', 'config', 'allowUpdates', 'repoURL', 'baseURL', 'selectedTask', 'searchIssuesURL', 'createIssueURL'],
   data: function () {
     return {
