acbe7801526906fedf73fb290fac45a6c67eaab0
Jesse <jesse@piascik.net>
2019-11-19 08:34:16 -0700

    Enable links to local files and images

--- src/utils/task-text-utils.js
+++ src/utils/task-text-utils.js
@@ -1,10 +1,13 @@
 import * as MarkdownIt from 'markdown-it'
 import * as cheerio from 'cheerio'
 import * as checkbox from 'markdown-it-checkbox'
+import * as emoji from 'markdown-it-emoji'
 import eol from 'eol'
+import path from 'path'
 
 const md = new MarkdownIt({html: true, breaks: true})
 md.use(checkbox)
+md.use(emoji)
 export default {
   text (task) {
     const html = md.render(task.getText({stripMeta: true, sanitize: true, stripTags: true, stripContext: true}))
@@ -20,7 +23,19 @@ export default {
     const html = md.render(description)
     const $ = cheerio.load(html)
     $('a').each(function () {
-      $(this).attr('target', '_blank')
+      const href = $(this).attr('href')
+      if (/\w+:\/\//.test(href)) return $(this).attr('target', '_blank')
+      const [filePath, line] = href.split(':')
+      $(this).attr('file-path', filePath)
+      $(this).attr('file-line', line)
+      $(this).attr('href', '#')
+    })
+    $('img').each(function () {
+      const src = $(this).attr('src')
+      if (!/\w+:\/\//.test(src)) {
+        const imgPath = path.join(task.repoId, src)
+        $(this).attr('src', `file://${imgPath}`)
+      }
     })
     // TODO:10 Support sorting of tasks in list id:43 +feature
     $('input[type=checkbox]').closest('li').css('list-style', 'none')
