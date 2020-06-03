a150fd7b4b0eaaa7fbcee2cd1699d8545ca9c693
Jesse <jesse@piascik.net>
2020-06-03 16:50:53 -0600

    Fix %20 issue

--- src/utils/task-text-utils.js
+++ src/utils/task-text-utils.js
@@ -29,7 +29,7 @@ function getEncodedDescription (description) {
   const encodedMD = encodeURIComponent(description)
   description = description
     .replace(CONTENT_TOKEN, encodedText)
-    .replace(/(?<!!)\[(.*?)\]\((.*)\)/g, (match, p1, p2) => { // URI encode file links
+    .replace(/(?<!!)\[(.*?)\]\((.*?)\)/g, (match, p1, p2) => { // URI encode file links
       return `[${p1}](${p2.replace(/\s/g, '%20')})`
     })
   return {encodedMD, encodedText, description}
